6d55520a779256a0358e8ff2919f3a8a5c5290d7
stefano.lattenero@studenti.unipd.it <97043416+StefanoLattenero@users.noreply.github.com>
2022-01-07 15:18:20 +0100

    ho modificato il file scacchiera

--- ProgettoScacchi/include/Scacchiera.h
+++ ProgettoScacchi/include/Scacchiera.h
@@ -1,6 +1,43 @@
+//Stefano Lattenero
+
 #ifndef SCACCHIERA_H
 #define SCACCHIERA_H
 
-class Scacchiera{}
+
+class Casella{
+    private:
+        int x;
+        int y;
+        bool empty;
+    public:
+        Casella(int a, int b)
+	        : y{a}, x{b} 
+        {}
+
+        bool isEmpty() { return empty; }
+};
+
+class Scacchiera{
+
+    private:
+        
+        const int x_MAX=7;
+        const int y_MAX=8;
+        object<object<Casella>> scac;
+
+        //enum y{ A , B , C , D , E , F , G , H };
+
+    public:
+        Scacchiera(int x_m, int y_m) 
+            : x_MAX{x_m}, y_MAX{y_m} 
+        {
+
+        }
+
+		void reset_scacchiera() {
+
+        }
+
+};
 
 #endif
